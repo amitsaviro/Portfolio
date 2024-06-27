04c0e78102b873a662ad1c85de91753acb10cecd
amitsaviro <saviramit@gmail.com>
2024-06-27 14:35:05 +0300

    responsive

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -11,9 +11,11 @@ const Intro = () => {
             <div className="introContent">
                 <span className="hello">Hello,</span>
                 <span className="introText">I'm <span className="introName">Amit</span><br />Full-Stack Developer</span>
+                <div className="introP">
                 <p className="introPara">I am Amit, a full-stack web developer, experienced in front-end technologies</p>
                 <p className="introPara"> like HTML, CSS, jQuery, JavaScript and React framework</p>
                 <p className="introPara"> And Java and Spring-Boot on the back-end with SQL-database.</p>
+                </div>
                 <Link to="contact" spy={true} smooth={true} offset={-100} duration={500}><button className="hire-me-btn"><FontAwesomeIcon className="hire-me-icon" icon={faBriefcase} /> Hire Me</button></Link>
             </div>
             <img className="amit" src={amit} alt="Amit" />
